cf0b71368c57a7894aa749ec025eb5f52fd5a6ab
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2023-11-20 18:33:14 -0400

    Adiciona palavras na lista

--- 1244 - Ordenação por Tamanho/1244.c
+++ 1244 - Ordenação por Tamanho/1244.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_SIZE 1024
 
 typedef struct Palavra
 {
@@ -13,19 +18,59 @@ int main(int argc, char const *argv[])
 
     while (testes > 0)
     {
-        char buffer[51]; //string + \0
-        scanf("%[^\n]s%*c", buffer);
+        palavra listaPalavras[50];
+
+        char entrada[MAX_INPUT_SIZE]; //string + \0
+        scanf("%[^\n]s%*c", entrada);
         //printf("%s\n", buffer);
 
-
-        // TODO
-        // Ajustar os casos onde há mais de um espaço sequencial
-        // Ajustar casos onde há espaços no começo e no final
-        int cursor = 0, contador = 1;
-        while (buffer[cursor] != '\0')
+        char buffer[256]; // Para armazenar temporariamente a palavra
+        int cursorEntrada = 0, contador = 0, cursorBuffer = 0, palavras = 0;
+        bool isPalavra = false;
+        do
         {
-            if (buffer[cursor] == ' ') contador += 1;
-            cursor += 1;
+            if (entrada[cursorEntrada] != ' ' && !isPalavra)
+            {
+                buffer[cursorBuffer] = entrada[cursorEntrada];
+                isPalavra = true;
+                palavras += 1;
+                contador += 1;
+                cursorBuffer += 1;
+            }
+            else if (entrada[cursorEntrada] == ' ' && isPalavra)
+            {
+                isPalavra = false;
+
+                // adiciona \0 no buffer, reinicia o cursorBuffer e adiciona a palavra na lista.
+                buffer[cursorBuffer] = '\0';
+                listaPalavras[palavras - 1].texto = malloc(strlen(buffer) + 1);
+                strcpy(listaPalavras[palavras - 1].texto, buffer);
+                listaPalavras[palavras - 1].tamanho = strlen(buffer);
+
+                cursorBuffer = 0;
+                
+            }
+            else if (isPalavra)
+            {
+                buffer[cursorBuffer] = entrada[cursorEntrada];
+                cursorBuffer += 1;
+            }
+            
+            cursorEntrada += 1;
+            
+        } while (entrada[cursorEntrada] != '\0');
+
+        if (isPalavra) // Então terminou o loop com uma palavra pendente
+        {
+            isPalavra = false;
+
+            // adiciona \0 no buffer, reinicia o cursorBuffer e adiciona a palavra na lista.
+            buffer[cursorBuffer] = '\0';
+            listaPalavras[palavras - 1].texto = malloc(strlen(buffer) + 1);
+            strcpy(listaPalavras[palavras - 1].texto, buffer);
+            listaPalavras[palavras - 1].tamanho = cursorBuffer;
+
+            cursorBuffer = 0;
         }
 
         printf("A quantidade de palavras encontradas é %d\n", contador);
